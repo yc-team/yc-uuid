c122eab9dbe2bd2eb98c4a1021752279c58290f1
zhangyaochun <zhangyaochun@wandoujia.com>
2015-03-03 15:46:00 +0800

    [command line] commit auto by @wd-deploy on 2015-03-03 03:46:00

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
 //source from https://gist.github.com/jed/982883
-function b(
-  a                  // placeholder
-){
-  return a           // if the placeholder was passed, return
-    ? (              // a random number from 0 to 15
-      a ^            // unless b is 8,
-      Math.random()  // in which case
-      * 16           // a random number from
-      >> a/4         // 8 to 11
-      ).toString(16) // in hexadecimal
-    : (              // or otherwise a concatenated string:
-      [1e7] +        // 10000000 +
-      -1e3 +         // -1000 +
-      -4e3 +         // -4000 +
-      -8e3 +         // -80000000 +
-      -1e11          // -100000000000,
-      ).replace(     // replacing
-        /[018]/g,    // zeroes, ones, and eights with
-        b            // random hex digits
-      )
-}
-
-module.exports = b;
+//change to https://gist.github.com/LeverOne/1308368
+module.exports = function(a,b){for(b=a='';a++<36;b+=a*51&52?(a^15?8^Math.random()*(a^20?16:4):4).toString(16):'-');return b}
